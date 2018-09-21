15c232798167f22562160f69f93b435a9f59a5e2
Robin de Mourat <robin.demourat@gmail.com>
2018-09-21 23:02:57 +0200

    fix copy-pasting issue in desktop version fix #1

--- main_process.js
+++ main_process.js
@@ -123,6 +123,25 @@ app.on('ready', async () => {
           }
         }]).popup(mainWindow);
       });
+    } else {
+      Menu.setApplicationMenu(
+        Menu.buildFromTemplate(
+          [
+            {
+              label: "Edit",
+              submenu: [
+                  { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+                  { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+                  { type: "separator" },
+                  { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+                  { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                  { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                  { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+              ]
+            }
+          ]
+          )
+        );
     }
 })
 
